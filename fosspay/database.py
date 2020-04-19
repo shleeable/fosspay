4257e0bb8304ddf1a20645156fe4a5fc5f503bf0
Shlee <github@shl.ee>
2020-04-19 16:56:24 +0800

    Update database.py

--- fosspay/database.py
+++ fosspay/database.py
@@ -1,9 +1,10 @@
+import sqlite3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from .config import _cfg, _cfgi
-engine = create_engine(_cfg('connection-string'))
+engine = create_engine(_cfg('connection-string'), module=sqlite3.dbapi2)
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
